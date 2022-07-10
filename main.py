1cc026506e1723c5487110aa81fd1eb49c04a33a
Ashley Hummel <ashley.l.hummel@gmail.com>
2022-07-10 00:11:49 -0700

    Collect creature prompt Scrape https://artprompts.org/creature-prompts/ and collect prompt through BeautifulSoup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import webbrowser
 import tweepy
-import logging
 
 # environment variables
 import os
@@ -8,7 +6,8 @@ import os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-logger = logging.getLogger()
+import requests
+from bs4 import BeautifulSoup
 
 # (code from https://realpython.com/twitter-bot-python-tweepy/ )
 # authenticate to Twitter
@@ -28,3 +27,11 @@ try:
     print("Authentication successful")
 except:
     print("Authentication failed")
+
+# GETTING CREATURE PROMPT FROM https://artprompts.org/creature-prompts/
+# (from https://www.freecodecamp.org/news/web-scraping-python-tutorial-how-to-scrape-data-from-a-website/ )
+page = requests.get('https://artprompts.org/creature-prompts/')
+
+soup = BeautifulSoup(page.content, 'html.parser')
+second_p_text = soup.select('p')[1].text # text of second p element (THE PROMPT)
+print(second_p_text)
