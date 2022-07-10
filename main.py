12c2b1dac716a682dc4aadd2d46a2b9a941ddec1
Ashley Hummel <ashley.l.hummel@gmail.com>
2022-07-09 21:11:42 -0700

    Environment variables passed into main.py are used to authenticate to Twitter. Verifying credentials is currently failing.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-#testing initial commit
+import tweepy
+import logging
+
+# environment variables
+import os
+# import .env file
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+logger = logging.getLogger()
+
+# (code from https://realpython.com/twitter-bot-python-tweepy/ )
+# authenticate to Twitter
+api_key = os.getenv("API_KEY")
+api_secret = os.getenv("API_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_secret = os.getenv("ACCESS_SECRET")
+
+
+auth = tweepy.OAuthHandler(api_key, api_secret)
+auth.set_access_token(access_token, access_secret)
+# create API object
+api = tweepy.API(auth)
+
+try:
+    api.verify_credentials()
+    print("Authentication successful")
+except:
+    print("Authentication failed")
