83b06044c45908f26b692a968a98960d55687491
Ashley Hummel <ashley.l.hummel@gmail.com>
2022-07-11 01:18:59 -0700

    Input and download generated image Inputs scraped prompt into DALLE mini and saves a generated image to specific folder

--- main.py
+++ main.py
@@ -9,8 +9,17 @@ load_dotenv(find_dotenv())
 import requests
 from bs4 import BeautifulSoup
 
+import urllib
+import urllib.request
+from selenium import webdriver # for navigating DALLE mini
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+import time
+
 # (code from https://realpython.com/twitter-bot-python-tweepy/ )
-# authenticate to Twitter
+# AUTHENTICATE TO TWITTER
 api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
 access_token = os.getenv("ACCESS_TOKEN")
@@ -29,9 +38,32 @@ except:
     print("Authentication failed")
 
 # GETTING CREATURE PROMPT FROM https://artprompts.org/creature-prompts/
-# (from https://www.freecodecamp.org/news/web-scraping-python-tutorial-how-to-scrape-data-from-a-website/ )
+# (code from https://www.freecodecamp.org/news/web-scraping-python-tutorial-how-to-scrape-data-from-a-website/ )
 page = requests.get('https://artprompts.org/creature-prompts/')
 
 soup = BeautifulSoup(page.content, 'html.parser')
-second_p_text = soup.select('p')[1].text # text of second p element (THE PROMPT)
-print(second_p_text)
+creature_prompt = soup.select('p')[1].text # text of second p element (THE PROMPT)
+print("Prompt:", creature_prompt)
+
+# INPUT PROMPT INTO DALLE MINI/CRAIYON (https://www.craiyon.com/)
+
+s = Service('/Applications/chromedriver') # changed to avoid DeprecationWarning
+driver = webdriver.Chrome(service=s)
+
+driver.get("https://www.craiyon.com/")
+
+prompt_input = driver.find_element(By.ID, "prompt")
+prompt_input.send_keys(creature_prompt)
+time.sleep(70) # delay (seconds) to let DALLE mini generate an image
+
+# RETRIEVE ONE OF THE IMAGES AND SAVE TO FOLDER
+
+images = driver.find_elements(By.XPATH, '//img') # gets list of images on DALLE mini site
+img_path = "/Users/ashleyhummel/Desktop/creature_bot_images"
+file_name = creature_prompt + ".png"
+fullfilename = os.path.join(img_path, file_name)
+
+src = images[3].get_attribute('src') # images[3] is the FIRST generated image
+urllib.request.urlretrieve(src, fullfilename) # save image to specific folder with a file name
+
+driver.quit()
